5dbcb5c5c91573d44379ed98f9192f5a43ff063b
Yash Dalmia <ydalmia@stanford.edu>
2021-06-12 23:06:58 -0700

    more cleanup

--- translation.jl
+++ translation.jl
@@ -2,34 +2,25 @@ using Chess
 
 ENCODING_TYPE = Tuple{SquareDelta, Union{PieceType, Nothing}}
 function generate_encoder_decoder()
-    move_list = Vector{ENCODING_TYPE}()
+    # queen moves
+    queen_deltas = [DELTA_N, DELTA_S, DELTA_E, DELTA_W, DELTA_NW, DELTA_NE, DELTA_SW, DELTA_SE]
+    queen_moves = [((n * d, nothing) for n in 1:7, d in queen_deltas)...] # flatten matrix by splatting
+    
+    # knight moves
+    delta_files = [DELTA_E, DELTA_W]
+    delta_ranks = [DELTA_N, DELTA_S]
+    knight_moves1 = [((df + dr, nothing) for df in 2*delta_files, dr in delta_ranks)...]
+    knight_moves2 = [((df + dr, nothing) for df in delta_files, dr in 2*delta_ranks)...]
+
+    # under-promotion moves
+    pawn_prmt_delta = [DELTA_N, DELTA_NE, DELTA_NW]
+    prmt_pieces = [KNIGHT, BISHOP, ROOK]
+    prmt_moves = [((d, piece) for d in pawn_prmt_delta, piece in prmt_pieces)...]
+    
+    move_list = cat(queen_moves, knight_moves1, knight_moves2, prmt_moves, dims=1)
     encoder = Dict{ENCODING_TYPE, Int64}()
     decoder = Dict{Int64, ENCODING_TYPE}()
 
-    # queen moves
-    for n in 1:7
-        for delta in [DELTA_N, DELTA_S, DELTA_E, DELTA_W, DELTA_NW, DELTA_NE, DELTA_SW, DELTA_SE]
-            push!(move_list, (n * delta, nothing))
-        end
-    end
-    # knight moves
-    for delta_file in [2 * DELTA_E, 2 * DELTA_W]
-        for delta_rank in [DELTA_N, DELTA_S]
-            push!(move_list, (delta_file + delta_rank, nothing))
-        end
-    end
-    # more knight moves (4)
-    for delta_file in [DELTA_E, DELTA_W]
-        for delta_rank in [2 * DELTA_N, 2 * DELTA_S]
-            push!(move_list, (delta_file + delta_rank, nothing))
-        end
-    end
-    # promotion moves (12)
-    for delta in [DELTA_N, DELTA_NE, DELTA_NW]
-        for piece in [KNIGHT, BISHOP, ROOK]
-            push!(move_list, (delta, piece))
-        end
-    end
     # encoder, decoder
     for (i, mv) in enumerate(move_list)
         encoder[mv] = i
@@ -38,6 +29,7 @@ function generate_encoder_decoder()
 
     return encoder, decoder
 end
+
 const encoder, decoder = generate_encoder_decoder()
 
 function encode(a, side::PieceColor)
@@ -51,7 +43,7 @@ function encode(a, side::PieceColor)
         end
 
         # elide queen promotions and pawns moving into the final rank of the board
-        # so that we only explicitly encode under promotions. ie, a queen promotion
+        # so that we only explicitly encode underpromotions. ie, a queen promotion
         # will have prmt = nothing
         prmt = nothing
         if ispromotion(x) && promotion(x) != QUEEN
@@ -103,7 +95,6 @@ function alphazero_rep(b::Board)
     )
     return convert(Array{Float32}, layers) # cast to float32 for faster ML
 end
-
 
 # black's pawn layer, it should have the pawns towards
 # the bottom of the screen if rotate works as intended
