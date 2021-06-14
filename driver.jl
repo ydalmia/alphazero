decd5951c6bfa470c4d04e51b411ef403e82d7e4
Elijah Kim <reachelijah@gmail.com>
2021-06-13 19:07:06 -0700

    06-13-2021 save train_x to file

--- driver.jl
+++ driver.jl
@@ -8,15 +8,19 @@ using Distributed
 addprocs(6) # adjust to the number of cores you have
 
 @everywhere using StatsBase
+@everywhere using JLD2
 @everywhere include("mcts.jl")
 
 function playgames(s=startboard()::Board; ngames=6, nsims=800)
+    # all_games_data = SharedArray{any}()
     @distributed for _ in 1:ngames
         root = TreeNode()
+        wholeTree = root
         train_x = []
 
         z = nothing
         while(true)
+            pprint(s, unicode=true, color=true)
             println("move simulated")
             if ischeckmate(s) 
                 z = 1 # lol vivek check this
@@ -31,7 +35,7 @@ function playgames(s=startboard()::Board; ngames=6, nsims=800)
                 π = N / sum(N)
 
                 # save example to train from later
-                push!(train_x, (s, π))
+                push!(train_x, (alphazero_rep(s), π))
 
                 # randomly sample child state according to π
                 idx = sample(1:length(N), ProbabilityWeights(π))
@@ -45,13 +49,16 @@ function playgames(s=startboard()::Board; ngames=6, nsims=800)
             z = -z
         end
 
-        println(train_x)
-        # print_tree(root, maxdepth=2)
+        # println(train_x)
+        print_tree(wholeTree, maxdepth=3)
+        core=myid()
+        # Serialization.serialize(open("games_data_core_$core.json", "w"), train_x)
+        @save "games_data_core_$core.jld2" train_x
+
     end
 end
 
-# @time fetch(playgames(fromfen("7k/4KRpp/4PP2/8/8/8/8/8 w - - 0 1")))
 # @time fetch(playgames(fromfen("8/1p1k1p1b/5PBr/8/2r5/1p2P1Q1/1KppR2R/8 w - - 0 1")))
-# @time fetch(playgames(fromfen("7k/4KRpp/4PP2/8/8/8/8/8 w - - 0 1")))
-
+# @time fetch(playgames(fromfen("7k/4KRpp/4PP2/8/8/8/8/8 w - - 0 1"), ngames=1)) # easy 1 move to checkmate
+# @time fetch(playgames(fromfen("kbK5/pp6/1P6/8/8/8/8/R7 w - - 0 1"), ngames=1, nsims=2000)) # 2 move to checkmate
 @time playgames()
